f6df2467e9ed82aeb14b9429b36d011db0f2b35c
HatulK <108576659+HatulK@users.noreply.github.com>
2022-11-23 19:25:23 +0200

    Add files via upload

--- advancedClassificationRecursion.c
+++ advancedClassificationRecursion.c
@@ -1,18 +1,20 @@
 #include "NumClass.h"
 
-int isPalindromeRecursive(int num ,int temp);
+int isPalindromeRecursive(int num, int temp);
 
-int isPalindrome(int num){
-    return num==isPalindromeRecursive(num,0);
-}
-int isPalindromeRecursive(int num ,int temp) {
-if(num==0) return temp;
-temp = (temp*10)+(num%10);
-return isPalindromeRecursive(num/10,temp);
+int isPalindrome(int num) {
+    return num == isPalindromeRecursive(num, 0);
 }
 
+int isPalindromeRecursive(int num, int temp) {
+    if (num == 0) return temp;
+    temp = (temp * 10) + (num % 10);
+    num = num / 10;
+    return isPalindromeRecursive(num, temp);
+}
 
-int isArmstrong (int num){
-    if (num<10) return num*num*num;
-    return (num%10)*(num%10)*(num%10)+ isArmstrong(num/10);
+
+int isArmstrong(int num) {
+    if (num < 10) return num * num * num;
+    return (num % 10) * (num % 10) * (num % 10) + isArmstrong(num / 10);
 }
